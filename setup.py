a7d6b11a6ca8f4e2b4f5375dbba8c01e5903cf2b
kamuridesu <myk.gata14@gmail.com>
2022-09-03 11:29:48 -0300

    Update package version to publish fix to pypi

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def requirements():
 
 
 setup(name="KamTube",
-      version = "0.0.2",
+      version = "0.0.3",
       author="Kamuri Amorim",
       author_email='luiz.k.amorim@gmail.com',
       url="https://github.com/kamuridesu/KamTube",
